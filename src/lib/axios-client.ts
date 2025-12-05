1ab621225acd2952981ed21f62c420c1e3937e4e
adibfuadi <adibfuadimuhammad@gmail.com>
2025-12-05 16:57:53 +0700

    set cookies cross site

--- src/lib/axios-client.ts
+++ src/lib/axios-client.ts
@@ -1,10 +1,10 @@
 import { CustomError } from "@/types/custom-error.type";
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL;
+// const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 const options = {
-  baseURL,
+  baseURL: "/api",
   withCredentials: true,
   timeout: 10000,
 };
